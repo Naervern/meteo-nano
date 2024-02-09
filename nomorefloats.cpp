ba4f81a93b6404fbc5832f83b8a7ad83e700b855
Antonio Maximiano <naervern@gmail.com>
2024-02-09 18:15:46 +0200

    Update nomorefloats.cpp

--- nomorefloats.cpp
+++ nomorefloats.cpp
@@ -2,7 +2,7 @@
 this is a small module for handling 4-byte floats that by far take unnecessary ram space, coming from cases such as temperature, humidity and pressure measurements where the sensor accuracy doesn't quite justify the capabilities of fp32 numbers
 */
 
-#include <stdio.h>
+#include <iostream>
 
 
 
